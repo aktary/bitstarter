bb107265404478b4b91bb7ec16005dc6afc615b9
David <aktaryda@gamil.com>
2014-02-16 00:19:26 +0000

    changed to read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var foo = Buffer.toString(fs.readFileSync("index.html"));
+  response.send(foo);
 });
 
 var port = process.env.PORT || 5000;
